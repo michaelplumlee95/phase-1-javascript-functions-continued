c4623c3c992b11b49df378602070700dbe05d9f2
Michael Plumlee <michaelplumlee95@gmail.com>
2023-03-13 16:07:26 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+wrapAdjective = function(highlight = '*') {
+    return function(adjective = 'special'){
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+    
+}
